0afd5744f6d71e9eb5eaba9d7f375232ee1cbfc4
Tomasz Jachna <tomasz.jachna.ext@nokia.com>
2017-03-01 12:03:36 +0100

    rename alpha

---
+++ rename.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <deque>
+#include <map>
+#include <vector>
+#include <random>
+
+int main() {
+
+/*
+    for (int i = 0; i < vec.size(); i++){    uzupelnianie z zakresu
+        vec[i] = i%3;
+    };
+
+    std::generate_n(std::back_inserter(d), 14, []{     uzupelnianie losowymi z zakresu
+        return rand() % 7;
+    });
+
+    std::ostream_iterator<int> out(std::cout, " ");         wypisywanie
+    std::copy(std::begin(vec), std::end(vec), out);
+
+    auto it = std::unique(std::begin(d), std::end(d));      unikaty
+    d.resize(std::distance(std::begin(d), it));
+*/
+
+    std::map <int, int> cipher;
+    std::vector<int> used;
+
+    for (int i = 0; i < 59; i++){
+        used[i] = i+65;
+    }
+
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(used.begin(), used.end(), g);
+
+
+    for (int i = 0; i < 59; i++){
+        cipher[i] = used[i];
+    }
+
+
+
+
+
+	return 0;
+}
